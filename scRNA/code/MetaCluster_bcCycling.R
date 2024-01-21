09a5d3fa72484bad4e2ea97ecfcf605c3c6fa298
lh64 <lh64@cornell.edu>
2024-01-20 20:27:53 -0600

    Adding the 'scRNA' directory from Corey's original repo into our repo. Everything from the original repo is now in this repo.

---
+++ scRNA/code/MetaCluster_bcCycling.R
@@ -0,0 +1,200 @@
+library(dplyr)
+library(ggplot2)
+library(Seurat)
+
+load("combined_includingState.RData")
+DimPlot(combined, reduction = "umap", group.by = "seurat_clusters")
+
+load("top25BCs.RData")
+bcs_top25 <- as.character(unique(bcNum_prop_compare_sub$Barcode))
+bc_not <- setdiff(combined@meta.data$lineage, bcs_top25)
+test <- combined@meta.data
+test$lineageColored[test$lineage %in% bcs_top25] <- test$lineage[test$lineage %in% bcs_top25]
+test$lineageColored[test$lineage %in% bc_not] <- "XXX"
+lineageColored <- subset(test, select = c("lineageColored"))
+combined <- AddMetaData(combined, lineageColored, col.name = "lineageColored")
+
+t2 <- as.data.frame(combined@meta.data)
+t2$BCnum <- as.integer(factor(t2$lineageColored, levels = unique(bcNum_prop_compare_sub$Barcode)))
+BCnum <- subset(t2, select = c("BCnum"))
+combined <- AddMetaData(combined, BCnum, col.name = "BCnum")
+
+df <- FetchData(combined, vars = c("UMAP_1", "UMAP_2", "orig.ident", "lineage", "lineageColored", "seurat_clusters", "BCnum"))
+# Convert Seurat clusters to meta-clusters
+df1 <- df %>% 
+  mutate(meta_cluster = case_when(
+    seurat_clusters %in% c("7") ~ 'UT_S',
+    seurat_clusters %in% c("6") ~ 'I_S',
+    seurat_clusters %in% c("0","2","4") ~ 'I_L',
+    TRUE ~ 'UT_L' ) )
+
+# Add large vs small information
+df2 <- df1 %>% 
+  mutate(cluster_size = case_when(
+    seurat_clusters %in% c("7","6") ~ 'Small',
+    TRUE ~ 'Large' ) )
+
+# # Percentages
+meta_cluster_UT_table_byBC <- subset(df1, orig.ident == "Untreated")[,c("meta_cluster", "BCnum")]
+meta_cluster_UT_byBC <- meta_cluster_UT_table_byBC %>%
+  dplyr::group_by(meta_cluster, BCnum) %>%
+  dplyr::summarise (n = n()) %>%
+  dplyr::group_by(BCnum) %>%
+  dplyr::mutate(freq = n / sum(n))
+meta_cluster_UT_byBC$Condition <- "Untreated"
+
+meta_cluster_I_table_byBC <- subset(df1, orig.ident == "Idling")[,c("meta_cluster", "BCnum")]
+meta_cluster_I_byBC <- meta_cluster_I_table_byBC %>%
+  dplyr::group_by(meta_cluster, BCnum) %>%
+  dplyr::summarise (n = n()) %>%
+  dplyr::group_by(BCnum) %>%
+  dplyr::mutate(freq = n / sum(n))
+meta_cluster_I_byBC$Condition <- "Idling"
+
+meta_cluster_byBC <- as.data.frame(rbind(meta_cluster_UT_byBC, meta_cluster_I_byBC))
+
+ggplot(subset(meta_cluster_byBC, Condition == "Untreated"), aes(x = as.factor(BCnum), y = freq,
+                                                         group = meta_cluster, fill = meta_cluster)) +
+  theme_bw() + geom_bar(position = position_fill(reverse = TRUE),
+                        stat = "identity", color = "black") +
+  scale_fill_manual(values = c("pink", "grey", "purple", "brown")) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x = "Barcode", y = "Percentage of Cells") +
+  ggtitle("Untreated Meta-cluster") +
+  theme(axis.text.y = element_text(size = 14),
+        legend.position = "bottom", legend.text = element_text(size = 14),
+        plot.title = element_text(size = 16, hjust = 0.5),
+        axis.text=element_text(size=14),
+        legend.title = element_blank(), axis.title=element_text(size=14),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggplot(subset(meta_cluster_byBC, Condition == "Idling"), aes(x = as.factor(BCnum), y = freq,
+                                                                group = meta_cluster, fill = meta_cluster)) +
+  theme_bw() + geom_bar(position = position_fill(reverse = TRUE),
+                        stat = "identity", color = "black") +
+  scale_fill_manual(values = c("pink", "grey", "purple", "brown")) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x = "Barcode", y = "Percentage of Cells") +
+  ggtitle("Idling Meta-cluster") +
+  theme(axis.text.y = element_text(size = 14),
+        legend.position = "bottom", legend.text = element_text(size = 14),
+        plot.title = element_text(size = 16, hjust = 0.5),
+        axis.text=element_text(size=14),
+        legend.title = element_blank(), axis.title=element_text(size=14),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+######
+
+# # Percentages
+meta_cluster_UT_table_byBC_x <- subset(df2, orig.ident == "Untreated")[,c("orig.ident", "BCnum", "cluster_size")]
+meta_cluster_UT_byBC_x <- meta_cluster_UT_table_byBC_x %>%
+  dplyr::group_by(orig.ident, cluster_size, BCnum) %>%
+  dplyr::summarise (n = n()) %>%
+  dplyr::group_by(BCnum) %>%
+  dplyr::mutate(freq = n / sum(n))
+
+meta_cluster_I_table_byBC_x <- subset(df2, orig.ident == "Idling")[,c("orig.ident", "BCnum", "cluster_size")]
+meta_cluster_I_byBC_x <- meta_cluster_I_table_byBC_x %>%
+  dplyr::group_by(orig.ident, cluster_size, BCnum) %>%
+  dplyr::summarise (n = n()) %>%
+  dplyr::group_by(BCnum) %>%
+  dplyr::mutate(freq = n / sum(n))
+
+meta_cluster_byBC_x <- as.data.frame(rbind(meta_cluster_UT_byBC_x, meta_cluster_I_byBC_x))
+meta_cluster_byBC_x <- subset(meta_cluster_byBC_x, BCnum != "8")
+
+meta_cluster_byBC_x$orig.ident <- factor(meta_cluster_byBC_x$orig.ident, levels = c("Untreated", "Idling"))
+
+ggplot(subset(meta_cluster_byBC_x, cluster_size == "Large"), aes(x = as.factor(BCnum), y = freq,
+                                                                group = orig.ident, fill = orig.ident)) +
+  theme_bw() + geom_bar(width=.5, position = "dodge",
+                        stat = "identity", color = "black") +
+  scale_fill_manual(values = c("purple", "pink")) +
+  scale_y_continuous(labels = scales::percent) +
+  coord_cartesian(ylim = c(0.65,0.95)) +
+  labs(x = "Barcode", y = "Percentage of Cells") +
+  ggtitle("Large Cluster in BRAFi") +
+  theme(axis.text.y = element_text(size = 14),
+        legend.position = "bottom", legend.text = element_text(size = 14),
+        plot.title = element_text(size = 16, hjust = 0.5),
+        axis.text=element_text(size=14),
+        legend.title = element_blank(), axis.title=element_text(size=14),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggplot(subset(meta_cluster_byBC_x, cluster_size == "Small"), aes(x = as.factor(BCnum), y = freq,
+                                                                 group = orig.ident, fill = orig.ident)) +
+  theme_bw() + geom_bar(width=.5, position = "dodge",
+                        stat = "identity", color = "black") +
+  scale_fill_manual(values = c("brown", "grey")) +
+  scale_y_continuous(labels = scales::percent) +
+  # coord_cartesian(ylim = c(0.65,0.95)) +
+  labs(x = "Barcode", y = "Percentage of Cells") +
+  ggtitle("Small Cluster in BRAFi") +
+  theme(axis.text.y = element_text(size = 14),
+        legend.position = "bottom", legend.text = element_text(size = 14),
+        plot.title = element_text(size = 16, hjust = 0.5),
+        axis.text=element_text(size=14),
+        legend.title = element_blank(), axis.title=element_text(size=14),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+##
+# ggplot(meta_cluster_byBC_x, aes(x = orig.ident, y = freq, fill = cluster_size)) +
+#   theme_bw() + geom_bar(width=.5,
+#                         stat = "identity", color = "black") +
+#   facet_wrap(vars(as.factor(BCnum)), ncol = 5) +
+#   scale_fill_manual(values = c("brown", "grey")) +
+#   scale_y_continuous(labels = scales::percent) +
+#   # coord_cartesian(ylim = c(0.65,0.95)) +
+#   labs(x = "", y = "Percentage of Cells") +
+#   # ggtitle("Cluster in BRAFi") +
+#   theme(axis.text.y = element_text(size = 12),
+#         legend.position = "none", legend.text = element_text(size = 12),
+#         plot.title = element_text(size = 16, hjust = 0.5),
+#         axis.text=element_text(size=12),
+#         legend.title = element_blank(), axis.title=element_text(size=12),
+#         panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+#         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+#   ggsave(".", width = 10, height =10)
+
+####### Correlation
+
+load("~/Documents/QuarantaLab/barcodeFC_forCorPlot.RData")
+names(test_hist1) <- c("FC", "Class", "BCnum", "lineage")
+bcFC <- test_hist1[,c("lineage", "BCnum", "FC")]
+
+##
+library(ggpubr)
+library(ggrepel)
+# BC-by-BC Number/Proportion of cells in each CC state
+stateBC_df <- FetchData(combined, vars = c("orig.ident", "lineage", "lineageColored", "State"))
+# stateBC_df <- subset(stateBC_df, lineageColored != "XXX")
+t1 <- stateBC_df
+t1$BCnum <- as.integer(factor(t1$lineageColored, levels = unique(bcNum_prop_compare_sub$Barcode)))
+t1$lineageColored <- factor(t1$lineageColored, levels = unique(bcNum_prop_compare_sub$Barcode))
+state_I_table_byBC <- subset(t1, orig.ident == "Idling")[,c("State", "BCnum", "lineage", "lineageColored")]
+state_I_byBC <- state_I_table_byBC %>% 
+  dplyr::group_by(State, BCnum) %>% 
+  dplyr::summarise (n = n()) %>%
+  dplyr::group_by(BCnum) %>%
+  dplyr::mutate(freq = n / sum(n))
+state_I_byBC$Condition <- "Idling"
+state_bc_I <- as.data.frame(subset(state_I_byBC, State == "Dividing"))
+state_bc_I <- subset(state_bc_I, BCnum != "NA")
+state_bc_I_sub <- state_bc_I[,c("BCnum", "freq")]
+
+compareBCs_top25 <- merge(bcFC, state_bc_I_sub, by = "BCnum")
+names(compareBCs_top25) <- c("BCnum", "lineage", "FC", "prop_I")
+
+ggscatter(compareBCs_top25, x="prop_I",y="FC", add="reg.line", size=1) +
+  stat_cor(method="pearson", aes(label = ..r.label..), size = 5) +
+  ggrepel::geom_label_repel(aes(label=BCnum)) +
+  scale_x_continuous(labels = scales::percent) +
+  labs(x="Percentage of Cycling Cells in Idling", 
+       y=expression(Log[2]~"Barcode Fold Change")) +
+  theme(axis.text = element_text(size = 14)) +
+  ggsave("Idling_bcFC_correlation.pdf", width = 6, height = 4)
---
+++ scRNA/code/VISION_SKMEL5.R
@@ -0,0 +1,82 @@
+library(VISION)
+library(Seurat)
+library(limma)
+library(stringr)
+
+load("combined_includingState.RData")
+
+## Run VISION Analysis
+counts <- as.data.frame(combined@assays$SCT@counts)
+meta <- as.data.frame(combined@meta.data)
+meta$lineage <- factor(meta$lineage)
+meta$lineageColored <- factor(meta$lineageColored)
+
+# Scale counts within a sample
+n.umi <- colSums(counts)
+scaled_counts <- t(t(counts) / n.umi) * median(n.umi)
+
+# HALLMARK GENES
+gmt_files <- list.files(path = '../data/VISION_hallmark/', pattern = "\\.gmt$")
+vis <- Vision(data = counts, signatures = gmt_files, meta = meta)
+vis <- analyze(vis)
+visScores <- as.data.frame(getSignatureScores(vis))
+
+## Plot overlay UMAP
+combined_VISION <- combined
+combined_VISION <- AddMetaData(object = combined, 
+                                          metadata = visScores)
+
+hallmark_metrics <- paste0("HALLMARK_", removeExt(gmt_files))
+
+combined_plotDat <- FetchData(combined_VISION, 
+                         vars = c("UMAP_1", "UMAP_2", "lineage", "Phase", "State", "old.ident",
+                                  hallmark_metrics, "HALLMARK_KRAS_SIGNALING",
+                                  "HALLMARK_UV_RESPONSE"))
+
+umap_vals <- as.data.frame(Embeddings(combined, reduction = "umap"))
+umap_vals_df <- dplyr::bind_rows(umap_vals)
+colnames(umap_vals_df) <- c("UMAP_1", "UMAP_2")
+pops_all <- subset(combined@meta.data, select = "orig.ident")
+umap_vals_df$Population <- unlist(pops_all)
+all_Kmeans <- kmeans(umap_vals_df[,1:2,], centers = 3)
+combined_plotDat$Kmeans <- all_Kmeans$cluster
+
+
+plt_all_overlay <- ggplot() + theme_bw() +
+  geom_density_2d(data = combined_plotDat, 
+                  aes(x = UMAP_1, y = UMAP_2,
+                      color = old.ident), alpha = 0.8) +
+  scale_color_manual(values = c("blue", "red")) +
+  geom_point(data = combined_plotDat, shape = 21,
+             aes(x = UMAP_1, y = UMAP_2, fill = HALLMARK_GLYCOLYSIS),
+             size = 0.8, stroke = 0.01) + 
+  scale_fill_gradient(guide = FALSE, 
+                      low = "white", 
+                      high = "black") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        axis.text = element_text(size = 12), legend.position = "none", 
+        legend.text = element_text(size = 12), legend.title = element_text(size=12), 
+        axis.title=element_text(size=12)) 
+
+## Plot distribution of all 50 hallmark sets for each population
+combined_dat_hallmarks <- combined_plotDat[7:ncol(combined_plotDat)]
+combined_dat_hallmarks_melt <- reshape2::melt(combined_dat_hallmarks, id.vars = "Kmeans")
+
+combined_dat_hallmarks_melt$variable <- str_remove(as.character(combined_dat_hallmarks_melt$variable), "HALLMARK_")
+combined_dat_hallmarks_melt$Kmeans <- factor(combined_dat_hallmarks_melt$Kmeans,
+                                            levels = c(1,2,3))
+
+plt_hallmarks <- ggplot(combined_dat_hallmarks_melt, aes(x=value, color = Kmeans, fill = Kmeans)) +
+  geom_density(alpha = 0.5) + facet_wrap(~variable, ncol = 6, scales = "free") + theme_bw() +
+  scale_color_manual(values = c("magenta", "gold", "skyblue"), 
+                     labels = c(1,2,3), name = "Clusters") +
+  scale_fill_manual(values = c("magenta", "gold", "skyblue"), 
+                     labels = c(1,2,3), name = "Clusters") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        legend.position = "right", axis.text = element_text(size = 18), axis.title = element_text(size = 18)) +
+  xlab("Signature Score") + ylab("Density") 
+
+plt_hallmarks + ggsave("allHallmarks_acrossSKMEL5clusters.svg", width = 18, height = 24)
+plt_hallmarks_leg <- ggpubr::get_legend(plt_hallmarks)
+as_ggplot(plt_hallmarks_leg) + ggsave("allHallmarks_acrossSKMEL5clusters_legend.svg",
+                                      width = 6, height = 4)
