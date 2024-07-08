ba1efd2b5550ca338fb8e3e8cf5d6b4e249fe8e3
lh64 <lh64@cornell.edu>
2024-07-08 15:19:33 -0500

    Added code to copy created figures from the source code folders over to the '_HAYFORD_2024_PAPER_PLOTS' directory and rename them to 'Fig_1A', 'Fig_1B', etc.

--- createrPlotsForPaper2024.R
+++ createrPlotsForPaper2024.R
@@ -1,5 +1,6 @@
 library(tools)
 library(knitr)
+library(hash)
 
 # Set working directory to directory where this script is
 if (Sys.getenv("RSTUDIO") == "1") {
@@ -36,21 +37,40 @@ uninstallReqdPkgs <- function(pkgs, directories){
   }
 }
 
-source('getReqdPkgs.r')
-
 # Some required packages
 otherpkgs <- c("glmGamPoi", "presto")
-getReqdPkgs(otherpkgs)
 
 # Paths to all directories
 directories <- c("ATAC/code", "barcoding/code", "dose_response/code",
                  "GO_correlations/code", "ion_flux/code", "RNA/code",
                  "scRNA/code")
 
+# Create the hash for copying plots created by the R scripts to the figure numbers for the paper
+figure_names <- hash()
+# ATAC/code
+# ...
+"barcoding/code"
+# ...
+"dose_response/code"
+# ...
+"GO_correlations/code"
+# ...
+"ion_flux/code"
+# ...
+"RNA/code"
+# ...
+# scRNA/code
+figure_names['UMAP_combined_SKMEL5_hg38_qcCCReg.svg'] <- 'Fig_1A.svg'
+figure_names['SKMEL5_allClusters_CellCycleState_proportion.pdf'] <- 'Fig_1E.pdf'
+# ...
+
 if (START_FROM_SCRATCH == TRUE)
   try(uninstallReqdPkgs(otherpkgs, directories))
 
-# Create the 'HAYFORD_2024_PAPER_PLOTS' directory if it doesn't already exist
+source('getReqdPkgs.r')
+getReqdPkgs(otherpkgs)
+
+# Create the '_HAYFORD_2024_PAPER_PLOTS' directory if it doesn't already exist
 if (!dir.exists("_HAYFORD_2024_PAPER_PLOTS")){
   dir.create("_HAYFORD_2024_PAPER_PLOTS")
 }
@@ -73,6 +93,7 @@ for (dir in directories){
       try(source(knitr::purl(file.path(dir, r_file), 
                              output=file.path(dir, outfile), 
                              quiet=TRUE), chdir=TRUE))
+      # remove temporarily created .R file
       if (file.exists(file.path(dir, outfile))){
         file.remove(file.path(dir, outfile))
       }
@@ -82,5 +103,15 @@ for (dir in directories){
     }
   }
   # Copy the files to the '_HAYFORD_2024_PAPER_PLOTS' directory
-  # ...
+  for (old_name in keys(figure_names)){
+    new_name <- values(figure_names, keys=old_name)
+    if (file.exists(file.path(dir, old_name))){
+      print(file.path(dir, old_name))
+      file.copy(file.path(dir, old_name), "_HAYFORD_2024_PAPER_PLOTS")
+      file.rename(file.path("_HAYFORD_2024_PAPER_PLOTS", old_name), 
+                  file.path("_HAYFORD_2024_PAPER_PLOTS", new_name))
+    }
+  }
 }
+
+
