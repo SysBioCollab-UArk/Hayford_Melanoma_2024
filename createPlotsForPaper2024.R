879e2c9a3abedfbb42673cc818e76375dc20a126
lh64 <lh64@cornell.edu>
2024-08-13 05:19:36 -0500

    Moved code for uninstalling and installing required packages from createPlotsForPaper2024.R into getReqdPkgs.r. Also, made a small change in createPlotsForPaper2024.R in how the files in the 'scRNA/code' are reordered to run Seurat_v5_SKMEL5_combined_hg38.R first. With these changes, the code should now be complete. Users can create the figures by running createPlotsForPaper2024.R. To do this, they can either run the getReqdPkgs.r script first, to install all required packages, or they can load the virtual environment by opening Hayford_Melanoma_2024.Rproj and then running renv::restore(). The only thing we still need to do in this repo is put these instructions into the README file.

--- createPlotsForPaper2024.R
+++ createPlotsForPaper2024.R
@@ -6,28 +6,19 @@ library(purrr)
 library(stringr)
 
 # Set working directory to the directory where this script is
-if (Sys.getenv("RSTUDIO") == "1") {
+if (Sys.getenv("RSTUDIO") == "1")
   setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-}
 
-# Clear the session memory 
-# (need to do this to prevent memory exhaustion while running the script)
+# Clear the session memory (need to do this to prevent memory exhaustion while 
+# running the script)
 rm(list=ls())
 
-# Set the following variable to TRUE if you want to remove all required packages
-# and run the script from scratch, reinstalling everything along the way
-# *****
-# NOTE: This basically works but often requires restarting the R session and
-# then rerunning the script with START_FROM_SCRATCH == FALSE. We may eventually
-# want to remove this in favor of a virtual environment --LAH 
-# START_FROM_SCRATCH <- FALSE
+# Set to TRUE to require the user to press [enter] before each script in the 
+# repo runs
+STEP_BY_STEP <- FALSE
 
-# Set the following variable to TRUE to run the script in 'debug' mode 
-# (i.e., press [enter] before each script in the repo runs)
-DEBUG <- FALSE
-
-# Create the hash for copying plots created by the R scripts to the figure 
-# numbers for the paper
+# Hash mapping the names of the plots created by the R scripts to the figure
+# numbers in the paper
 figure_names <- hash()
 
 # ATAC/code
@@ -76,35 +67,6 @@ figure_names['Untreated_smallClusterGO_BP.pdf'] <- 'Fig_S1B.pdf'
 figure_names['Idling_smallClusterGO_BP.pdf'] <- 'Fig_S1C.pdf'
 figure_names['UMAP_combined_lineageID_tinted_BCs_others.svg'] <- 'Fig_S2D.svg'
 
-# Function to find all required packages in an R or Rmd script
-# findReqdLibraries <- function(dir, r_file){
-#   x <- grep('library\\(', readLines(file.path(dir, r_file)), value=TRUE)
-#   y <- grep('#', x, value=TRUE, invert=TRUE) # remove commented out libraries
-#   z <- sub("^\\s*library\\((.+)\\)\\s*$", "\\1", y) # get library names
-#   return(z)
-# }
-
-# Function to uninstall all required packages so script can be run from scratch
-# uninstallReqdPkgs <- function(pkgs, directories){
-#   message("Uninstalling all required packages")
-#   remove.packages(pkgs)
-#   # Loop over all directories
-#   for (dir in directories){
-#     message(paste("Changing directory to:", dir))
-#     r_files <- list.files(dir, "[:.:](r|R|Rmd)$")
-#     # Loop over R and Rmd scripts
-#     for (r_file in r_files){
-#       message(paste("Uninstalling packages from:", r_file))
-#       # Uninstall all required packages
-#       z <- findReqdLibraries(dir, r_file)
-#       remove.packages(z)
-#     }
-#   }
-# }
-
-# Some required packages
-# otherpkgs <- c("glmGamPoi", "presto")
-
 # Paths to all directories
 directories <- c(file.path("barcoding", "code"), 
                  file.path("dose_response", "code"),
@@ -114,20 +76,11 @@ directories <- c(file.path("barcoding", "code"),
                  file.path("scRNA", "code"), 
                  file.path("ATAC", "code"))
 
-# Uninstall all required packages if starting from scratch
-# if (START_FROM_SCRATCH == TRUE)
-#   try(uninstallReqdPkgs(otherpkgs, directories))
-
-# Install all required packages
-# source('getReqdPkgs.r')
-# getReqdPkgs(otherpkgs)
-
 # Create the '_HAYFORD_2024_PAPER_PLOTS' directory if it doesn't already exist
 if (!dir.exists("_HAYFORD_2024_PAPER_PLOTS")){
   dir.create("_HAYFORD_2024_PAPER_PLOTS")
-  for (subdir in c("FIG_1", "FIG_2", "FIG_3", "FIG_4", "FIG_S1", "FIG_S2", "FIG_S3")){
+  for (subdir in c("FIG_1", "FIG_2", "FIG_3", "FIG_4", "FIG_S1", "FIG_S2", "FIG_S3"))
     dir.create(file.path("_HAYFORD_2024_PAPER_PLOTS", subdir))
-  }
 }
 
 ########## RUN ALL R AND Rmd SCRIPTS ##########
@@ -140,49 +93,41 @@ while (!is.null(dev.list()))
 for (dir in directories){
   message(paste("Changing directory to:", dir))
   r_files <- list.files(dir, "[:.:](r|R|Rmd)$")
-  if (dir == 'scRNA/code'){ # run Seurat_v5_SKMEL5_combined_hg38.R first
-    r_files <- c(r_files[-1], r_files[1]) 
-  }
-  
+  if (dir == 'scRNA/code') # run Seurat_v5_SKMEL5_combined_hg38.R first
+    r_files <- c("Seurat_v5_SKMEL5_combined_hg38.R", 
+                 r_files[r_files != "Seurat_v5_SKMEL5_combined_hg38.R"])
+
   # Loop over R and Rmd scripts
   for (r_file in r_files){
     message(paste("Running script:", r_file))
-    
+
     # To prevent memory exhaustion, delete all objects created in the previous 
     # R script before running the next one
-    if (dir == directories[1]){
+    if (dir == directories[1])
       # Don't delete objects from the main script
       save_list <- c(rep(ls()), "save_list") 
-    }
-    else{
+    else
       # Delete all objects in the workspace except the ones in the main script
       rm(list=ls()[!(ls() %in% save_list)])
-    }
     
     # This pauses the code before each script in the repo runs, so the user
     # can keep track of what's going on
-    if (DEBUG == TRUE) readline(prompt="Press [enter] to continue")
-    
-    # Get all required libraries
-    # z <- findReqdLibraries(dir, r_file)
-    # getReqdPkgs(z)
+    if (STEP_BY_STEP) readline(prompt="Press [enter] to continue")
     
     # Run the R or Rmd script
     if (file_ext(r_file) == 'Rmd'){
       outfile <- sub(".Rmd", ".R", r_file)
       try(source(knitr::purl(file.path(dir, r_file),
-                             output=file.path(dir, outfile),
-                             quiet=TRUE), chdir=TRUE))
+                             output=file.path(dir, outfile), quiet=TRUE), 
+                 chdir=TRUE))
       # remove temporarily created .R file
-      if (file.exists(file.path(dir, outfile))){
+      if (file.exists(file.path(dir, outfile))) 
         file.remove(file.path(dir, outfile))
-      }
     }
-    else{ # r or R script
+    else # r or R script
       try(source(file.path(dir, r_file), chdir=TRUE))
-    }
   }
-  
+
   # Clear all plots from the console
   while (!is.null(dev.list())) 
     dev.off()
--- getReqdPkgs.r
+++ getReqdPkgs.r
@@ -1,38 +1,106 @@
+# Set working directory to the directory where this script is
+if (Sys.getenv("RSTUDIO") == "1")
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+# Function to find all required packages in an R or Rmd script
+findReqdLibraries <- function(dir, r_file){
+  x <- grep('library\\(', readLines(file.path(dir, r_file)), value=TRUE)
+  y <- grep('^\\s*#', x, value=TRUE, invert=TRUE) # remove commented out libraries
+  z <- sub("^\\s*library\\((.+)\\)\\s*$", "\\1", y) # get library names
+  return(z)
+}
+
+# Function to uninstall all required packages so script can be run from scratch
+uninstallReqdPkgs <- function(pkgs, directories){
+  message("Uninstalling all required packages")
+  remove.packages(pkgs)
+  # Loop over all directories
+  for (dir in directories){
+    message(paste("Changing directory to:", dir))
+    r_files <- list.files(dir, "[:.:](r|R|Rmd)$")
+    # Loop over R and Rmd scripts
+    for (r_file in r_files){
+      message(paste("Uninstalling packages from:", r_file))
+      # Uninstall all required packages
+      z <- findReqdLibraries(dir, r_file)
+      remove.packages(z)
+    }
+  }
+}
+
+# Function to install required packages
 getReqdPkgs <- function(req_pkgs) {
     if (!requireNamespace("BiocManager", quietly = TRUE))
-        install.packages("BiocManager")
+      install.packages("BiocManager")
     
     inst_pkgs <- installed.packages()[,"Package"]
-    
     needed_pkgs <- setdiff(req_pkgs,inst_pkgs)
-    std_pkgs <- needed_pkgs[needed_pkgs %in% 
-                                tools::CRAN_package_db()[,"Package"]]
+    std_pkgs <- needed_pkgs[
+      needed_pkgs %in% tools::CRAN_package_db()[,"Package"]]
     bioc_pkgs <- needed_pkgs[needed_pkgs %in% BiocManager::available()]
     other_pkgs <- setdiff(needed_pkgs,c(std_pkgs,bioc_pkgs))
-    
+
     if(!length(other_pkgs)==0) {
       message(cat("Packages not found in CRAN or BioC still needed:",
                     paste(other_pkgs, collapse=", ")))
       require(devtools)
       # Install VISION package
-      if ('VISION' %in% other_pkgs){
-        install_github("YosefLab/VISION")
-      }
+      if ('VISION' %in% other_pkgs) 
+        install_github("YosefLab/VISION", quiet=TRUE)
       # Install presto package
-      if ('presto' %in% other_pkgs){
-        install_github("immunogenomics/presto")
-      }
+      if ('presto' %in% other_pkgs)
+        install_github("immunogenomics/presto", quiet=TRUE)
       # Install diprate package
-      if ('diprate' %in% other_pkgs){
-        install_github("QuLab-VU/dipDRC", subdir="diprate", dependencies=TRUE)
-      }
+      if ('diprate' %in% other_pkgs)
+        install_github("QuLab-VU/dipDRC", subdir="diprate", dependencies=TRUE, 
+                       quiet=TRUE)
     }
-    
+
     if(length(std_pkgs)==0 & length(bioc_pkgs)==0) {
-        message('All required CRAN and BioC packages have previously been installed')
-    } else {
-        if(!is.null(std_pkgs)) install.packages(std_pkgs)
-        if(!is.null(bioc_pkgs)) BiocManager::install(bioc_pkgs)
+      message('All required CRAN and BioC packages have previously been installed')
+    } 
+    else {
+      if(!is.null(std_pkgs)) install.packages(std_pkgs)
+      if(!is.null(bioc_pkgs)) BiocManager::install(bioc_pkgs, ask=FALSE)
     }
+}
+
+##### MAIN SCRIPT #####
+
+# Set to TRUE if you want to remove all required packages and run the script 
+# from scratch, reinstalling everything along the way
+START_FROM_SCRATCH <- FALSE
+
+# Paths to all directories
+directories <- c(file.path("barcoding", "code"), 
+                 file.path("dose_response", "code"),
+                 file.path("GO_correlations", "code"), 
+                 file.path("ion_flux", "code"), 
+                 file.path("RNA", "code"),
+                 file.path("scRNA", "code"), 
+                 file.path("ATAC", "code"))
+
+# Some required packages
+otherpkgs <- c("glmGamPoi", "presto")
+
+# Uninstall all required packages if starting from scratch
+if (START_FROM_SCRATCH){
+  try(uninstallReqdPkgs(otherpkgs, directories))
+  # reinstall the following packages, which are needed to run this script
+  install.packages("purrr")
+  install.packages("stringr")
+}
+
+# Install all required packages
+getReqdPkgs(otherpkgs)
+# Loop over all directories
+for (dir in directories){
+  message(paste("Changing directory to:", dir))
+  r_files <- list.files(dir, "[:.:](r|R|Rmd)$")
+  # Loop over R and Rmd scripts
+  for (r_file in r_files){
+    message(paste("Inspecting script:", r_file))
+    # Get all required libraries
+    getReqdPkgs(findReqdLibraries(dir, r_file))
+  }
 }
